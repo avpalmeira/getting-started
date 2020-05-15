9ebc3f307d90dbc1f5485a57e4340cb0a3d3d1dc
Antonio Palmeira <antonio@palmeira.tech>
2020-05-15 19:13:18 -0300

    show all notes in a list view

--- xamarin-notes/Notes/Notes/App.xaml.cs
+++ xamarin-notes/Notes/Notes/App.xaml.cs
@@ -1,16 +1,21 @@
 ﻿using System;
+using System.IO;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
 
 namespace Notes
 {
     public partial class App : Application
     {
+        public static string FolderPath { get; private set; }
+
         public App()
         {
             InitializeComponent();
+            FolderPath = Path.Combine(
+                    Environment.GetFolderPath(
+                        Environment.SpecialFolder.LocalApplicationData));
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new NotesPage());
         }
 
         protected override void OnStart()
